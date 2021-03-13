96e573a8a91616ff997b0d15b938b2f8a27e784f
DivyanshuPandey1702 <divyanshupandey1702@gmail.com>
2021-03-13 17:32:06 +0530

    Added gitignore, procfile and updated ports

--- app.js
+++ app.js
@@ -90,6 +90,11 @@ app.post("/submitted", function(req, res) {
   });
 })
 
-app.listen(3000, function() {
-  console.log("server started at port 3000 ...");
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("server has started ...");
 })
